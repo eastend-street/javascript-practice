192751044a35fd9b35157cd94b2af934293adf00
eastend-street <ss_1322irf@icloud.com>
2019-05-27 14:02:41 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let person = "Steve Jobs";
+// window.alert(person);
+
+// another way of showing element
+/*
+This is
+multi-line comment
+*/
+
+document.write(
+  "<h1>The name of the person who created Apple is " + person + "</h1>"
+);
