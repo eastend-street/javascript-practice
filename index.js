e51ecf44b12d89c7671bf25b682595e1c608521e
eastend-street <ss_1322irf@icloud.com>
2019-05-27 14:31:47 -0700

    implement 1-100 total

--- index.js
+++ index.js
@@ -10,3 +10,25 @@ multi-line comment
 document.write(
   "<h1>The name of the person who created Apple is " + person + "</h1>"
 );
+
+total = 0;
+even_total = 0;
+odd_total = 0;
+for (number = 1; number < 101; number++) {
+  total = total + number;
+  if (number % 2 === 0) {
+    even_total = even_total + number;
+  } else if (number % 2 === 1) {
+    odd_total = odd_total + number;
+  }
+}
+
+document.write(
+  "<p> The total of 1-100 is " +
+    total +
+    ". <br>The even total is " +
+    even_total +
+    ". <br>The odd total is " +
+    odd_total +
+    ". </p>"
+);
