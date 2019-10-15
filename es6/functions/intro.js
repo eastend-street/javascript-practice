518e74934cdbd5ba3b2c0b76e6990171f23d74e4
eastend-street <ss_1322irf@icloud.com>
2019-10-15 14:05:55 -0700

    added es6 directory

---
+++ es6/functions/intro.js
@@ -0,0 +1,19 @@
+// before es6
+var name = "before es6";
+console.log({ name });
+
+name = "changed before es6";
+console.log({ name });
+
+// ES6
+let animal = "dog-let";
+console.log({ animal });
+
+animal = "cat";
+console.log({ animal });
+
+const animalConst = "dog-const";
+console.log({ animalConst });
+
+animalConst = "cat";
+console.log({ animalConst }); // error
