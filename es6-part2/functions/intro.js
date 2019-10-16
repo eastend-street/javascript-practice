6e57a3c6c27dcc5747a950333e726b6d6dd396ee
eastend-street <ss_1322irf@icloud.com>
2019-10-16 14:16:37 -0700

    added whichIsYoung function

--- es6-part2/functions/intro.js
+++ es6-part2/functions/intro.js
@@ -34,8 +34,16 @@ const classObj = [
   null
 ];
 
+const whichIsYoung = (person1, person2) => {
+  return person1.age < person2.age
+    ? `${person1.name} is younger than ${person2.name}`
+    : `${person2.name} is younger than ${person1.name}`;
+};
+
 console.log(classObj[3]);
 console.table(classObj);
 
 // null and undefined are false when using boolean
 console.log(classObj[3] ? "true" : "false");
+
+console.log(whichIsYoung(classObj[1], classObj[2]));
