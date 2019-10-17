2560f8db3553e2df81599d02b3f2ec11af60f7c0
eastend-street <ss_1322irf@icloud.com>
2019-10-17 14:19:36 -0700

    added slice fuction

--- es6-part3/functions/intro.js
+++ es6-part3/functions/intro.js
@@ -18,13 +18,13 @@ const filteredResult = students.filter(student => student.length < 4);
 console.log(filteredResult);
 
 // splice ---------------------------------
-const sweets = [
-  "chocolate",
-  "cake",
-  "bubble tea",
-  "cheese cake",
-  "potato chips"
-];
+let sweets = ["chocolate", "cake", "bubble tea", "cheese cake", "potato chips"];
 
 sweets.splice(0, 2, "Hot chocolate");
 console.log(sweets);
+
+// slice ---------------------------------
+let animals = ["lion", "mink", "parrots", "cats", "snake", "rabbit"];
+
+const slicedAnimals = animals.slice(2, 4);
+console.log(animals, slicedAnimals);
