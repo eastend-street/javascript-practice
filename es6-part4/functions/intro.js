c841b5afb804ad8428a9c910072e9a8fd9e833ec
eastend-street <ss_1322irf@icloud.com>
2019-10-18 14:07:16 -0700

    add rest parameter

--- es6-part4/functions/intro.js
+++ es6-part4/functions/intro.js
@@ -43,3 +43,24 @@ console.log("set", set);
 set.forEach((value, value2, set) => {
   console.log("forEach", value, value2, set);
 });
+
+// Rest parameters
+
+const addition = (className, ...students) => {
+  console.log(`These are students for the ${className} class:`, students);
+};
+
+addition("WMAD", "Shin", "James", "Ayako");
+
+const addition2 = (className, ...students) => {
+  students.map(student => {
+    student.gender === "female" && console.log("female student: ", student);
+  });
+};
+
+addition2(
+  "WMAD2",
+  { name: "Shin", gender: "male" },
+  { name: "Ayako", gender: "female" },
+  { name: "James", gender: "male" }
+);
