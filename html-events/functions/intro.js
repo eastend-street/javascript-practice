9d1788fdf04eefb8dec0bdccab7dac4412132566
eastend-street <ss_1322irf@icloud.com>
2019-10-18 16:04:32 -0700

    add html task list

--- html-events/functions/intro.js
+++ html-events/functions/intro.js
@@ -6,6 +6,17 @@ const handleFormClick = e => {
   e.preventDefault();
 
   const target = e.target;
-  console.log(target);
-  console.log("Clicked!");
+  // console.log(target);
+
+  const tagName = e.target;
+  // console.log(tagName.tagName);
+
+  const inputValue = document.getElementById("nameInput").value;
+  console.log(inputValue);
+
+  let newLine = document.createElement("li");
+  newLine.textContent = inputValue;
+  
+  let list = document.getElementById("nameList");
+  list.appendChild(newLine);
 };
