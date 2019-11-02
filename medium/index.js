3c981d14e13690b917327ca5e90093fabd367ce7
eastend-street <ss_1322irf@icloud.com>
2019-11-02 14:54:21 -0700

    changed medium index.js

--- medium/index.js
+++ medium/index.js
@@ -6,21 +6,12 @@ const sampleConst = 3; // Okay, this will be never reassigned
 let sampleLet = 4; // It will be reassigned, I'll be careful!
 sampleLet = 5;
 
-
-
-
 console.log("1" == 1); // true. because it doesn't check the data type.
 console.log("1" === 1); // false. It checks the data type.
 
-
-
-
-
-
-
 const name = "David";
 const age = 30;
-const place= "Vancouver";
+const place = "Vancouver";
 
 console.log(name + " is " + age + " years old, living in " + place + ".");
 console.log(`${name} is ${age} years old, living in ${place}.`);
